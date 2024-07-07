6f2a91b837800212aa338ede03748df64c0b7cb4
nick <nicola.andrei.g@gmail.com>
2024-07-07 17:12:44 +0300

    am modificat functiile putin

--- autocorelation.py
+++ autocorelation.py
@@ -1,66 +1,70 @@
 import math
 import sys  # helps me forcibly stop my program.
 
+
 # Null hypothesis H0: The generated binary sequence is (pseudo)random.
 # Alternative hypothesis H1: The generated binary sequence is not (pseudo)random.
-def check_len(v, n):
-  if len(v) < n:
-    print("You did not enter a sequence of the correct length")
-    return 1
-  return 0
+def check_len(_v: bytes, _n: int) -> bool:
+    if len(_v) < _n:
+        print("You did not enter a sequence of the correct length")
+        return False
+    return True
 
-def check_interval(x, y):
-  if not(x >= 1 and x <= y and y <= n//2):
-    print("The interval format is not correct")
-    return 1
-  return 0
+
+def check_interval(_x: int, _y: int) -> bool:
+    if not (1 <= _x <= _y <= n // 2):
+        print("The interval format is not correct")
+        return False
+    return True
+
 
 print("Enter the desired length of the bit sequence:")
 n = int(input())
 n_str = str(n)
 print("Enter the sequence of " + n_str + " bits:")
-arr = input()  
+arr = input().strip()
 # Here it takes the entire line in which I read the elements
 # which I read separated by a space, and then map them to integers 
 # in the vector
-v = list(map(int, arr.split(' ')))  
-if check_len(v, n) == 1:
-  sys.exit()  # If the input v is not correct, exit forcibly.
+v = list(int(bit) for bit in arr)
+if not check_len(bytes(v), n):
+    sys.exit(1)  # If the input v is not correct, exit forcibly.
 
 print("Enter the significance level alpha:")
 alpha = float(input())
 
-x, y = map(int, input("Enter a valid interval of natural numbers. The left end must be greater than 1, and the right end less than n/2\n ").split())
-if check_interval(x, y) == 1:
-  sys.exit()
+x, y = map(int, input("Enter a valid interval of natural numbers. The left end must be greater than 1, and the right "
+                      "end less than " + str(n//2) + '\n').split())
+if not check_interval(x, y):
+    sys.exit(2)
 # Print the interval
 print("Left end:", x)
 print("Right end:", y)
 ok = 1
 for d in range(x, y + 1, 1):
-  # d will be the length of the left shift
-  new_secv = [0] * (n - d)
-  sum = 0
-  # In the vector new_secv I will store the sequence processed with the xor operator.
-  for i in range(len(new_secv)):
-    new_secv[i] = v[i] ^ v[i + d] 
-    # Applied xor between the corresponding bits.
-    new_secv[i] = 2 * new_secv[i] - 1
-    # Converted the bits from 0 to -1, while the 1 bits remained 1.
-    sum = sum + new_secv[i]
-  sum_abs = abs(sum)
-  print(sum_abs)
-  sum_abs = sum_abs / math.sqrt(n - d)
-  # In sum_abs I  will score the test statistic
-  p_value = math.erfc(sum_abs / math.sqrt(2))
-  print(p_value)
-  if p_value < alpha:
-    print("The null hypothesis is not accepted at the significance level provided: " + str(alpha))
-    ok = 0
-    break
+    # d will be the length of the left shift
+    new_secv = [0] * (n - d)
+    sum = 0
+    # In the vector new_secv I will store the sequence processed with the xor operator.
+    for i in range(len(new_secv)):
+        new_secv[i] = v[i] ^ v[i + d]
+        # Applied xor between the corresponding bits.
+        new_secv[i] = 2 * new_secv[i] - 1
+        # Converted the bits from 0 to -1, while the 1 bit remained 1.
+        sum = sum + new_secv[i]
+    sum_abs = abs(sum)
+    print(sum_abs)
+    sum_abs = sum_abs / math.sqrt(n - d)
+    # In sum_abs I  will score the test statistic
+    p_value = math.erfc(sum_abs / math.sqrt(2))
+    print(p_value)
+    if p_value < alpha:
+        print("The null hypothesis is not accepted at the significance level provided: " + str(alpha))
+        ok = 0
+        break
 
 # If the for loop does not exit with a forced break, the null hypothesis is accepted,
-# otherwise, the following two lines are not executed, the hypothesis has already been analyzed
+# otherwise, the following two lines are not executed, the hypothesis has already been analyzed,
 # and it was concluded that it is not accepted because the condition was not met.
 if ok == 1:
-  print("The null hypothesis is accepted at the significance level provided: " + str(alpha))
+    print("The null hypothesis is accepted at the significance level provided: " + str(alpha))
--- serial.py
+++ serial.py
@@ -3,50 +3,53 @@ from scipy.special import gammainc
 import math
 import sys  # helps me forcibly stop my program.
 
+
 # Null hypothesis H0: The generated binary sequence is (pseudo)random.
 # Alternative hypothesis H1: The generated binary sequence is not (pseudo)random.
-def check_len(v, n):
-  if len(v) < n:
-    print("You did not enter a sequence of the correct length")
-    return 1
-  return 0
+def check_len(v, _n: int) -> bool:
+    if len(v) < _n:
+        print("You did not enter a sequence of the correct length")
+        return False
+    return True
 
-def check_m(m, n):
-  if m > (math.log2(n) - 2):
-    print("The number m does not meet the mandatory condition!")
-    return 1
-  return 0
 
-def build_secv(v, m):
-    n = len(v)
-    sequences = []
-    for i in range(1, 4):
-        if m - i > 0:
-          new_secv = v + v[0:m-i]
-          sequences.append(new_secv)
-        elif m - i == 0:
-          new_secv  = v
-          sequences.append(new_secv)
+def check_m(_m: int, _n: int) -> bool:
+    if _m > (math.log2(_n) - 2):
+        print("The number m does not meet the mandatory condition!")
+        return False
+    return True
+
+
+def build_secv(_v, _m: int) -> list[int]:
+    seqs = []
+    for _i in range(1, 4):
+        if _m - _i > 0:
+            new_secv = _v + _v[0:_m - _i]
+            seqs.append(new_secv)
+        elif _m - _i == 0:
+            new_secv = _v
+            seqs.append(new_secv)
         else:
-          break
-    return sequences
+            break
+    return seqs
+
 
 def found_pattern(v, m):
-   n = len(v)
-   pattern_dictionary  = {}
-   # Take all patterns of length m and add them to a dictionary
-   for i in range(n - m + 1):
-      pattern = tuple(v[i:i+m])
-      if pattern in pattern_dictionary:
-        pattern_dictionary[pattern] +=1
-        # Increase the frequency of the pattern in the dictionary, i.e., increase the key's value.
-      else:
-        pattern_dictionary[pattern] = 1
+    n = len(v)
+    pattern_dictionary = {}
+    # Take all patterns of length m and add them to a dictionary
+    for i in range(n - m + 1):
+        pattern = tuple(v[i:i + m])
+        if pattern in pattern_dictionary:
+            pattern_dictionary[pattern] += 1
+            # Increase the frequency of the pattern in the dictionary, i.e., increase the key's value.
+        else:
+            pattern_dictionary[pattern] = 1
 
-   # The keys are the patterns, the values are the counts associated with the patterns.
-   # .items() generates the pair of iterators (key, value)
-   found_patterns = {pattern: count for pattern, count in pattern_dictionary.items() if count > 1}
-   return found_patterns
+    # The keys are the patterns, the values are the counts associated with the patterns.
+    # .items() generates the pair of iterators (key, value)
+    found_patterns = {pattern: count for pattern, count in pattern_dictionary.items() if count > 1}
+    return found_patterns
 
 
 print("Enter the desired length of the bit sequence:")
@@ -58,16 +61,16 @@ v = list(map(int, arr.split(' ')))
 # Here it takes the entire line in which I read the elements
 # which I read separated by a space, and then map them to integers
 # in the vector
-if check_len(v, n) == 1:
-  sys.exit()  # If the input v is not correct, exit forcibly.
+if not check_len(v, n):
+    sys.exit()  # If the input v is not correct, exit forcibly.
 
 print("Enter the significance level alpha:")
 alpha = float(input())
 
 print("Enter the desired length of the patterns to check, m: ")
 m = int(input())
-if check_m(m, n) == 1:
-  sys.exit()
+if not check_m(m, n):
+    sys.exit()
 
 sequences = build_secv(v, m)
 
@@ -85,24 +88,24 @@ for i, sequence in enumerate(sequences):
     found_patterns = found_pattern(sequence, m_new)
     # print(found_patterns)
     for pattern, frequency in found_patterns.items():
-      # Display the key + its value => items
-      print(f"Pattern: {pattern}, frequency: {frequency}")
-      sum = sum + math.pow(frequency, 2)
-      functions[ct] = ((math.pow(2, m_new))/n) * sum - n
+        # Display the key + its value => items
+        print(f"Pattern: {pattern}, frequency: {frequency}")
+        sum = sum + math.pow(frequency, 2)
+        functions[ct] = ((math.pow(2, m_new)) / n) * sum - n
     print(functions[ct])
     print("\n")
     m_new = m_new - 1
     ct = ct + 1
 for i in range(0, 3):
-  print(functions[i])
-  # If an element of the functions vector is not populated, it was set to 0 by the initial assignments [0] * 3
+    print(functions[i])
+    # If an element of the functions vector is not populated, it was set to 0 by the initial assignments [0] * 3
 statistic_1 = 0
 statistic_2 = 0
 statistic_1 = functions[0] - functions[1]
 statistic_2 = functions[0] - 2 * functions[1] + functions[2]
 
-p_value1 = gammainc(math.pow(2, m - 2), statistic_1/2)
-p_value2 = gammainc(math.pow(2, m - 3), statistic_2/2)
+p_value1 = gammainc(math.pow(2, m - 2), statistic_1 / 2)
+p_value2 = gammainc(math.pow(2, m - 3), statistic_2 / 2)
 print(statistic_1)
 print(statistic_2)
 print(p_value1)
@@ -110,6 +113,6 @@ print(p_value2)
 # If both conditions are met, the null hypothesis is accepted,
 # otherwise it is not.
 if p_value1 > alpha and p_value2 > alpha:
-  print("The null hypothesis is accepted at the significance level provided: " + str(alpha))
+    print("The null hypothesis is accepted at the significance level provided: " + str(alpha))
 else:
-  print("The null hypothesis is not accepted at the significance level provided: " + str(alpha))
+    print("The null hypothesis is not accepted at the significance level provided: " + str(alpha))
