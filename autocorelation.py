9f99f08f68a4195a1803f39cc4972b5a90d57e1c
banescuema <77149092+banescuema@users.noreply.github.com>
2024-07-07 14:38:34 +0300

    Update(1) autocorelation.py
    I changed the comments.

--- autocorelation.py
+++ autocorelation.py
@@ -1,66 +1,66 @@
 import math
-import sys  
-# ma ajuta sa imi opresc fortat programul.
+import sys  # helps me forcibly stop my program.
 
-# Ipoteza de nul 0 H : secvența binară generată este (pseudo)aleatoare.
-# Ipoteza alternativă A H : secvența binară generată nu este (pseudo)aleatoare.
+# Null hypothesis H0: The generated binary sequence is (pseudo)random.
+# Alternative hypothesis H1: The generated binary sequence is not (pseudo)random.
 def check_len(v, n):
   if len(v) < n:
-    print("Nu ati citit secventa de lungimea corecta")
+    print("You did not enter a sequence of the correct length")
     return 1
   return 0
 
 def check_interval(x, y):
   if not(x >= 1 and x <= y and y <= n//2):
-    print("Formatul intervalului nu este unul corect")
+    print("The interval format is not correct")
     return 1
   return 0
 
-print("Introduceti lungimea dorita a secventei de biti:")
+print("Enter the desired length of the bit sequence:")
 n = int(input())
 n_str = str(n)
-print("Introduceti secventa de " + n_str + " biti:")
+print("Enter the sequence of " + n_str + " bits:")
 arr = input()  
-# aici imi preia toata linia in care citesc elementele
-# pe care le citesc separate printr-un spatiu, si apoi la mapez in inturi 
-# in cadrul vectorului
+# Here it takes the entire line in which I read the elements
+# which I read separated by a space, and then map them to integers 
+# in the vector
 v = list(map(int, arr.split(' ')))  
 if check_len(v, n) == 1:
-  sys.exit()  # in caz ca nu am citit corect v, se iese fortat.
+  sys.exit()  # If the input v is not correct, exit forcibly.
 
-print("Introduceti nivelul de semnificatie alfa:")
-alfa = float(input())
+print("Enter the significance level alpha:")
+alpha = float(input())
 
-x, y = map(int, input("Introduceti un interval valid de numere naturale. Capatul din stanga trebuie sa fie mai mare ca 1, iar cel din dreapta mai mic ca n/2\n ").split())
+x, y = map(int, input("Enter a valid interval of natural numbers. The left end must be greater than 1, and the right end less than n/2\n ").split())
 if check_interval(x, y) == 1:
   sys.exit()
-# printez si intervalul
-print("Capatul din stanga:", x)
-print("Capatul din dreapta:", y)
+# Print the interval
+print("Left end:", x)
+print("Right end:", y)
 ok = 1
 for d in range(x, y + 1, 1):
-  # d va fi lungimea shiftului la stanga
+  # d will be the length of the left shift
   new_secv = [0] * (n - d)
   sum = 0
-  # in vectorul new_secv voi memora secventa prelucrata cu operatorul xor.
+  # In the vector new_secv I will store the sequence processed with the xor operator.
   for i in range(len(new_secv)):
     new_secv[i] = v[i] ^ v[i + d] 
-    # am aplicat xor intre bitii aferenti.
+    # Applied xor between the corresponding bits.
     new_secv[i] = 2 * new_secv[i] - 1
-    # am transformat bitii de 0 in -1, iar cei de 1 au ramas 1.
+    # Converted the bits from 0 to -1, while the 1 bits remained 1.
     sum = sum + new_secv[i]
   sum_abs = abs(sum)
   print(sum_abs)
   sum_abs = sum_abs / math.sqrt(n - d)
+  # In sum_abs I  will score the test statistic
   p_value = math.erfc(sum_abs / math.sqrt(2))
   print(p_value)
-  if p_value < alfa:
-    print("Nu se accepta ipoteza de nul la nivelul de semnificatie primit ca intrare de " + str(alfa))
+  if p_value < alpha:
+    print("The null hypothesis is not accepted at the significance level provided: " + str(alpha))
     ok = 0
     break
 
-# daca nu se iese din bucla for cu break, fortat, ipoteza de nul se accepta,
-# in caz contrar, nu se executa urmatoarele doua linii, ipoteza deja fiind analizata
-# si ajungand la concluzia ca nu se accepta pentru ca nu se indeplineste conditia.
+# If the for loop does not exit with a forced break, the null hypothesis is accepted,
+# otherwise, the following two lines are not executed, the hypothesis has already been analyzed
+# and it was concluded that it is not accepted because the condition was not met.
 if ok == 1:
-  print("Se accepta ipoteza de nul la nivelul de semnificatie primit ca intrare de " + str(alfa))
+  print("The null hypothesis is accepted at the significance level provided: " + str(alpha))
