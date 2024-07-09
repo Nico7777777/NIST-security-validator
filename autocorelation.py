489ee03358cd3c15d0a6b4d462fe5ccfcfce9613
banescuema <77149092+banescuema@users.noreply.github.com>
2024-07-09 12:19:13 +0300

    Am dat parametrii necesari functiei autocorrelation(), pentru a o putea include in main.py

--- autocorelation.py
+++ autocorelation.py
@@ -1,15 +1,10 @@
 import math
-import sys  # helps me forcibly stop my program.
 
-
-# Null hypothesis H0: The generated binary sequence is (pseudo)random.
-# Alternative hypothesis H1: The generated binary sequence is not (pseudo)random.
 def check_len(_v: bytes, _n: int) -> bool:
     if len(_v) < _n:
         print("You did not enter a sequence of the correct length")
         return False
     return True
-
 
 def check_interval(_x: int, _y: int, _n: int) -> bool:
     if not (1 <= _x <= _y <= _n // 2):
@@ -17,56 +12,33 @@ def check_interval(_x: int, _y: int, _n: int) -> bool:
         return False
     return True
 
-
-def autocorelation():
-    print("Enter the desired length of the bit sequence:")
-    n = int(input())
-    n_str = str(n)
-    print("Enter the sequence of " + n_str + " bits:")
-    arr = input().strip()
-    # Here it takes the entire line in which I read the elements
-    # which I read separated by a space, and then map them to integers
-    # in the vector
+def autocorrelation(n, arr, alpha, x, y):
     v = list(int(bit) for bit in arr)
     if not check_len(bytes(v), n):
-        sys.exit(1)  # If the input v is not correct, exit forcibly.
-
-    print("Enter the significance level alpha:")
-    alpha = float(input())
-
-    x, y = map(int,
-               input("Enter a valid interval of natural numbers. The left end must be greater than 1, and the right "
-                     "end less than " + str(n // 2) + '\n').split())
+        return "Error: Sequence length is not correct"
+    
     if not check_interval(x, y, n):
-        sys.exit(2)
-    # Print the interval
-    print("Left end:", x)
-    print("Right end:", y)
+        return "Error: Interval is not correct"
+
+    result = ""
     ok = 1
-    for d in range(x, y + 1, 1):
-        # d will be the length of the left shift
+    for d in range(x, y + 1):
         new_secv = [0] * (n - d)
         sum = 0
-        # In the vector new_secv I will store the sequence processed with the xor operator.
         for i in range(len(new_secv)):
             new_secv[i] = v[i] ^ v[i + d]
-            # Applied xor between the corresponding bits.
             new_secv[i] = 2 * new_secv[i] - 1
-            # Converted the bits from 0 to -1, while the 1 bit remained 1.
             sum = sum + new_secv[i]
         sum_abs = abs(sum)
-        print(sum_abs)
         sum_abs = sum_abs / math.sqrt(n - d)
-        # In sum_abs I  will score the test statistic
         p_value = math.erfc(sum_abs / math.sqrt(2))
-        print(p_value)
+        result += f"Calculated p_value is: {p_value}\n"
         if p_value < alpha:
-            print("The null hypothesis is not accepted at the significance level provided: " + str(alpha))
+            result += f"The null hypothesis is not accepted at the significance level {alpha}\n"
             ok = 0
             break
 
-    # If the for loop does not exit with a forced break, the null hypothesis is accepted,
-    # otherwise, the following two lines are not executed, the hypothesis has already been analyzed,
-    # and it was concluded that it is not accepted because the condition was not met.
     if ok == 1:
-        print("The null hypothesis is accepted at the significance level provided: " + str(alpha))
+        result += f"The null hypothesis is accepted at the significance level {alpha}\n"
+    
+    return result
